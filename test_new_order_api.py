a30988133517f3a0bf86fb06c17b9f4f9d2ca30f
orhuner <orhuner@gmail.com>
2022-01-28 19:40:44 -0500

    added negative tests

--- test_new_order_api.py
+++ test_new_order_api.py
@@ -6,6 +6,7 @@ import json
 import time
 import requests
 import time
+import pytest
 
 
 class TestNewOrderEndpoint:
@@ -64,10 +65,8 @@ class TestNewOrderEndpoint:
 
         return response
 
-    # SIDE TESTS: Tests for the side parameter and other combinations
     def test_create_successful_buy_order(self):
         # validate order is buy side limit order and check status of order
-        print(self.url)
         payload = self.create_payload(self.url, "btcusd", "5", "3633.00", "buy", "exchange limit", ["maker-or-cancel"])
         request_headers = self.set_request_headers(self.gemini_api_key, payload)
         response = self.place_new_order(self.url, payload, request_headers)
@@ -132,7 +131,6 @@ class TestNewOrderEndpoint:
         assert response.status_code == 200
         response_body = response.json()
         assert response_body["side"] == "sell"
-        assert response_body["symbol"] == "btcusd"
         assert response_body["type"] == "exchange limit"
 
     def test_empty_options_standard_limit_order_request(self):
@@ -142,10 +140,21 @@ class TestNewOrderEndpoint:
         assert response.status_code == 200
         response_body = response.json()
         assert response_body["side"] == "sell"
-        assert response_body["symbol"] == "btcusd"
-        assert response_body["type"] == "exchange limit"
         assert response_body["options"] == []
         assert response_body["is_cancelled"] == False
+
+    def test_original_and_remaining_amount(self):
+        # Without any execution the remaining amount should be the same as original amount
+        payload = self.create_payload(self.url, "btcusd", "10", "40000.00", "sell", "exchange limit", [], "42000.00")
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        assert response.status_code == 200
+        response_body = response.json()
+        assert response_body["side"] == "sell"
+        assert response_body["options"] == []
+        assert response_body["is_cancelled"] == False
+        assert response_body["original_amount"] == "10"
+        assert response_body["remaining_amount"] == "10"
 
     def test_immediate_or_cancel_order(self):
         # Assumption: Since there are no other orders on the orderbook, the order won't be filled and cancelled.
@@ -156,8 +165,6 @@ class TestNewOrderEndpoint:
         assert response.status_code == 200
         response_body = response.json()
         assert response_body["side"] == "sell"
-        assert response_body["symbol"] == "btcusd"
-        assert response_body["type"] == "exchange limit"
         assert response_body["options"] == ["immediate-or-cancel"]
         assert response_body["is_cancelled"] == True
 
@@ -169,43 +176,36 @@ class TestNewOrderEndpoint:
         response = self.place_new_order(self.url, payload, request_headers)
         assert response.status_code == 200
         response_body = response.json()
-        print(response_body)
         assert response_body["side"] == "sell"
-        assert response_body["symbol"] == "btcusd"
-        assert response_body["type"] == "exchange limit"
         assert response_body["options"] == ['fill-or-kill']
         assert response_body["is_cancelled"] == True
 
-    # def test_indication_of_interest_order_type(self):
-    #     payload = self.create_payload(self.url, "btcusd", "5", "40000.00", "sell", "exchange limit", ["indication-of-interest"])
-    #     request_headers = self.set_request_headers(self.gemini_api_key, payload)
-    #     response = self.place_new_order(self.url, payload, request_headers)
-    #     assert response.status_code == 200
-    #     response_body = response.json()
-    #     assert response_body["side"] == "sell"
-    #     assert response_body["symbol"] == "btcusd"
-    #     assert response_body["type"] == "exchange limit"
-    #     assert response_body["options"] == ['indication-of-interest']
-    #     assert response_body["is_cancelled"] == True
-    #
-    # def test_auction_only_order_type(self):
-    #     payload = self.create_payload(self.url, "btcusd", "5", "3633.00", "buy", "exchange limit", ["auction-only"])
-    #     request_headers = self.set_request_headers(self.gemini_api_key, payload)
-    #     response = self.place_new_order(self.url, payload, request_headers)
-    #     assert response.status_code == 200
-    #     response_body = response.json()
-    #     print(response_body)
-    #     assert response_body["side"] == "sell"
-    #     assert response_body["symbol"] == "btcusd"
-    #     assert response_body["type"] == "exchange limit"
-    #     assert response_body["options"] == ['auction-only']
-    #     assert response_body["is_cancelled"] == True
+    @pytest.mark.skip(reason="receiving 400 with exchange limit orders")
+    def test_auction_only_order_type(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "3655.00", "sell", "exchange limit", ["auction-only"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        assert response.status_code == 200
+        response_body = response.json()
+        assert response_body["side"] == "sell"
+        assert response_body["options"] == ['auction-only']
+        assert response_body["is_cancelled"] == True
+
+    @pytest.mark.skip(reason="receiving 400 with exchange limit orders")
+    def test_indication_of_interest(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "3655.00", "sell", "exchange limit", ["indication-of-interest"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        assert response.status_code == 200
+        response_body = response.json()
+        assert response_body["side"] == "sell"
+        assert response_body["options"] == ['indication-of-interest']
+        assert response_body["is_cancelled"] == True
 
     # Negative test cases
-    def test_against_replay_attack(self):
-        # test that a non increasing nonce can't be used in a second order
+    def test_non_increasing_nonce(self):
+        # test against a replay attack by attempting to use a non increasing nonce value for second order attempt
         nonce = self.set_nonce()
-        time.sleep(.5)
         payload = {
             'request': '/v1/order/new',
             'nonce': nonce,
@@ -226,19 +226,108 @@ class TestNewOrderEndpoint:
                            'X-GEMINI-SIGNATURE': signature,
                            'Cache-Control': "no-cache"}
         response = self.place_new_order(self.url, payload, request_headers)
+        # first order should be ok with a valid nonce
         assert response.status_code == 200
         response = self.place_new_order(self.url, payload, request_headers)
+        response_body = response.json()
+        # second order has invalid nonce so 400 error message is sent by server
         assert response.status_code == 400
+        assert response_body['reason'] == "InvalidNonce"
+        assert response_body['message'] == "Nonce '{}' has not increased since your last call to the Gemini API.".format(nonce)
 
-    def test_stop_price_must_be_lower_than_price_for_buy(self):
-        payload = self.create_payload(self.url, "btcusd", "5", "40000.00", "buy", "exchange limit", [], "42000.00")
-        print(payload)
+    def test_low_limit_sell_order(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "2.00", "sell", "exchange limit",
+                                      ["maker-or-cancel"])
         request_headers = self.set_request_headers(self.gemini_api_key, payload)
         response = self.place_new_order(self.url, payload, request_headers)
         assert response.status_code == 200
         response_body = response.json()
-        print(response_body)
-        assert response_body["side"] == "buy"
-        assert response_body["symbol"] == "btcusd"
-        assert response_body['is_cancelled'] == True
-        assert response_body['reason'] == "MakerOrCancelWouldTake"
+        assert response_body["is_cancelled"] == True
+
+    def test_high_limit_buy_order(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "200000.00", "buy", "exchange limit",
+                                      ["maker-or-cancel"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        assert response.status_code == 200
+        response_body = response.json()
+        assert response_body["is_cancelled"] == True
+        assert response_body["is_live"] == False
+        assert response_body["reason"] == 'MakerOrCancelWouldTake'
+
+    def test_invalid_price(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "-40000.00", "sell", "exchange limit",
+                                      ["maker-or-cancel"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        response_body = response.json()
+        assert response.status_code == 400
+        assert response_body["reason"] == "InvalidPrice"
+
+    def test_invalid_side_btcusd(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "40000.00", "test", "exchange limit",
+                                      ["maker-or-cancel"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        response_body = response.json()
+        assert response.status_code == 400
+        assert response_body["reason"] == "InvalidSide"
+        assert response_body["message"] == "Invalid side for symbol BTCUSD: 'test'"
+
+    def test_invalid_side_ethusd(self):
+        payload = self.create_payload(self.url, "ethusd", "5", "40000.00", "test", "exchange limit",
+                                      ["maker-or-cancel"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        response_body = response.json()
+        assert response.status_code == 400
+        assert response_body["reason"] == "InvalidSide"
+        assert response_body["message"] == "Invalid side for symbol ETHUSD: 'test'"
+
+    def test_multiple_options_are_not_permitted(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "200000.00", "buy", "exchange limit",
+                                      ["maker-or-cancel", "fill-or-kill"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        response_body = response.json()
+        assert response.status_code == 400
+        assert response_body["reason"] == "ConflictingOptions"
+        assert response_body["message"] == "A single order supports at most one of these options: ['maker-or-cancel', " \
+                             "'immediate-or-cancel', 'auction-only', 'fill-or-kill']"
+
+    def test_invalid_options(self):
+        payload = self.create_payload(self.url, "btcusd", "5", "40000.00", "buy", "exchange limit",
+                                      ["test"])
+        request_headers = self.set_request_headers(self.gemini_api_key, payload)
+        response = self.place_new_order(self.url, payload, request_headers)
+        response_body = response.json()
+        assert response.status_code == 400
+        assert response_body["reason"] == "UnsupportedOption"
+        assert response_body["message"] == 'Option "test" is not supported.'
+
+    def test_invalid_signature(self):
+        nonce = self.set_nonce()
+        payload = {
+            'request': '/v1/order/new',
+            'nonce': nonce,
+            'symbol': 'btcusd',
+            'amount': '5',
+            'price': '40000.00',
+            'side': 'buy',
+            'type': 'exchange limit',
+            'options': ["maker-or-cancel"]
+        }
+
+        encoded_payload = self.encode_payload(payload)
+        signature = hmac.new(self.gemini_api_secret, encoded_payload, hashlib.sha384).hexdigest()
+        request_headers = {'Content-Type': "text/plain",
+                           'Content-Length': "0",
+                           'X-GEMINI-APIKEY': "account-ABCDF6Fvt20Px9SLyXYZ",
+                           'X-GEMINI-PAYLOAD': encoded_payload,
+                           'X-GEMINI-SIGNATURE': signature,
+                           'Cache-Control': "no-cache"}
+        response = self.place_new_order(self.url, payload, request_headers)
+        response_body = response.json()
+        assert response.status_code == 400
+        response_body['reason'] = 'InvalidSignature'
+        response_body['message'] = 'InvalidSignature'
